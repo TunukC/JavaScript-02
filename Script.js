2c030a92650bec33ba89a22e0a6ebb8eb69f6cc3
Ana <83139776+TunukC@users.noreply.github.com>
2021-10-22 21:27:17 +0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,32 @@
+// хранения имени пользователя  //
+// хранения адреса пользователя  //
+
+//let curentUserName;
+//let curentUserAdres;
+
+//let num1 = 200;
+//let num2 = 2021;
+
+//document.write(num1 +  num2 + "<br>");
+
+//let userName = prompt("Your Name");
+//let userAge = prompt("your Age");
+//let userAdress = prompt("your Adress");
+//let userCountry = prompt("Your Country");
+
+//document.write("<h2>Your Name : " + userName + "</h2>");
+//document.write("<h2>your Age : " + userAge + "</h2>");
+//document.write("<h2>your Adress : " + userAdress + "</h2> ");
+//document.write("<h2>Your Country :  " + userCountry +"</h2>");
+
+//const pi = 3.14;
+//document.write(pi  + 0.01 );
+
+
+// Самостоятельная работа
+let userName = prompt("Your Name");
+let userAdress = prompt("your Adress");
+let userCountry = prompt("Your Country");
+
+document.write("<h2> Your name : " + A.C + "</h2>" + "<br>");
+
